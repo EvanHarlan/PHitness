4cf776f35a76f41ef6401d488ed1097b4d089ffb
Lzobz <91154803+Lzobz@users.noreply.github.com>
2025-03-04 16:47:30 -0600

    added connection for mongo

---
+++ backend/connect.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require("mongodb");
+ 
+// Replace the following with your Atlas connection string                                                                                                                                        
+const url = "mongodb+srv://zobelluke:bqgNcEa46pDU8Kdy@phitnesscluster.f3qur.mongodb.net/?retryWrites=true&w=majority&appName=PHitnessCluster";
+// Connect to your Atlas cluster
+const client = new MongoClient(url);
+async function run() {
+    try {
+        await client.connect();
+        console.log("Successfully connected to Atlas");
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
