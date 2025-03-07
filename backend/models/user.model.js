a8ef87e3617da3b0f024871b1fecbde1a22516f8
Adamedo04 <lunn.adam764@gmail.com>
2025-03-07 13:21:34 -0600

    backend dev

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,2 +1,40 @@
-// This is where we will create the schema for storing user information
-// Username, Email, password (hashed + salted), array of friends, user id, etc.
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// Hash the password before saving it to the database
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next(); // Only hash if password is modified or new
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Method to compare entered password with stored hashed password
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
